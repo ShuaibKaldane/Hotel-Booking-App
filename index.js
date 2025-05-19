83a2fd1b33d147547cb79d8f841ba31de529ec13
Shuaib Kaldane <shuaib.kaldane@vit.edu.in>
2025-05-19 21:50:14 +0530

    CRUD Operations

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const app = express();
 const List = require("./modules/Listening.js");
 const path = require("path");
+const Listening = require('./modules/Listening.js');
+const methodOverride = require("method-override");
+
 
 
 app.listen(8080, ()=>{
@@ -21,7 +24,13 @@ async function main() {
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded ({extended : true}))
+app.use(express.urlencoded ({extended : true}));
+app.use(methodOverride("_method"));
+
+// Root Route
+app.get("/", (req , res)=>{
+  res.send("Welcome to the home page")
+})
 
 // Index Route
 app.get("/alllist", async(req , res)=>{
@@ -30,9 +39,47 @@ app.get("/alllist", async(req , res)=>{
 
 })
 
+// Create Route
+app.get("/listing/new" , (req , res)=>{
+  res.render("listening/create.ejs")
+
+})
+
+app.post("/listening/response", async (req , res)=>{
+  console.log(req.body.listing)
+  const listening = new Listening(req.body.listing);
+  await listening.save();
+  res.redirect("/alllist")
+
+})
+
 // Read Route
 app.get("/listing/:id", async (req , res)=>{
   let {id}= req.params;
   let show = await List.findById(id);
   res.render("listening/show.ejs", {show})
+})
+
+// Update Route
+app.get("/listings/:id/edit", async (req ,res)=>{
+  let {id}= req.params;
+  let show = await List.findById(id);
+  res.render("listening/Edit.ejs" , {show});
+
+})
+
+app.put("/listing/:id", async(req , res)=>{
+  let {id} = req.params;
+  await Listening.findByIdAndUpdate(id , {...req.body.listing});
+  res.redirect(`/listining/${id}`)
+
+})
+
+// Delete Route
+app.delete("/listing/:id", async(req , res)=>{
+  let {id} = req.params;
+  let deletedList = await Listening.findByIdAndDelete(id);
+  console.log(deletedList); 
+  res.redirect("/alllist")
+
 })
